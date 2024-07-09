d7f1aca4007ff7ddf46327e781e25a24588c882d
Code and Debug <codeanddebugedu@gmail.com>
2024-07-09 19:59:11 +0530

    patterns logic buildup

---
+++ 6. For Loop/loop-else.py
@@ -0,0 +1,15 @@
+# for i in range(1, 11):
+#     if i == 4:
+#         break
+#     print(i)
+# else:
+#     print("Done")
+
+i = 1
+while i <= 10:
+    if i == 4:
+        break
+    print(i)
+    i += 1
+else:
+    print("Done")
