0b394be3cfd3d3fbcb2a41723f0751b1a67a48ba
Code and Debug <codeanddebugedu@gmail.com>
2024-07-24 19:46:06 +0530

    dictionary basic

---
+++ 14. Dictionary/basic.py
@@ -0,0 +1,13 @@
+# Dictionary (Key-Value) Unique, Ordered
+my_dict = {
+    "name": "Anirudh",
+    "age": 18,
+    "gender": "Male",
+    100: 200,
+    "name": "Xyz",
+    2: 100,
+}
+
+
+print(my_dict)
+print(type(my_dict))
